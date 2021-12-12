f7f259dc58738c8be76dc26d16967ccbf500542a
Astarina Maulida <astarina.maulida@gmail.com>
2021-12-12 04:38:39 -0500

    Add link in AboutPage to / homepage

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,18 +1,15 @@
-// Step 1: Import React
 import * as React from 'react'
+import { Link } from 'gatsby'
 
-// Step 2: Define your component
 const AboutPage = () => {
   return (
     <main>
       <title>About Me</title>
       <h1>About Me</h1>
-      <p>Hi there! I'm the proud creator of this site. I love to drink brown sugar boba milk tea.
-        I love traveling and learning new languages. Let's connect!
-      </p>
+      <Link to="/">Back to Home</Link>
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
     </main>
   )
 }
 
-// Step 3: Export your component
 export default AboutPage
