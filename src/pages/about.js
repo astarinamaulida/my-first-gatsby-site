e75e658a3c34f7394c2282944f32b51d6f7644d0
Astarina Maulida <astarina.maulida@gmail.com>
2021-12-12 04:32:14 -0500

    Create about component

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+// Step 1: Import React
+import * as React from 'react'
+
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <main>
+      <title>About Me</title>
+      <h1>About Me</h1>
+      <p>Hi there! I'm the proud creator of this site. I love to drink brown sugar boba milk tea.
+        I love traveling and learning new languages. Let's connect!
+      </p>
+    </main>
+  )
+}
+
+// Step 3: Export your component
+export default AboutPage
