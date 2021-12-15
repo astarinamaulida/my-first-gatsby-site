d3058943096b92f5106398ddf0df42e2ab4aff27
Astarina Maulida <astarina.maulida@gmail.com>
2021-12-15 03:33:54 -0500

    Try to reinstall plugin after previous error

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,10 @@ import { StaticImage } from 'gatsby-plugin-image'
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <p>Hi, I am Astarina Maulida.
+        A Fullstack Developer.</p>
       <StaticImage
-        alt="Hi, I am Astarina Maulida.
-        A Fullstack Developer"
+        alt=""
         src="../images/photoportfolio.png"
       />
     </Layout>
